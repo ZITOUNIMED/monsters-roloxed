0389b2e2a3fc4ba819b49b39c4875e7d4602e920
Mohamed Zitouni <zitouni.mohamed.email@gmail.com>
2019-09-13 10:13:58 +0200

    Updates

---
+++ precache-manifest.1077b5c55a715c8539a9a1fa4e66927b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3407e61a85b827069dec82e431065025",
+    "url": "/ZITOUNIMED/monsters-roloxed.git/index.html"
+  },
+  {
+    "revision": "bd6c746c027d38b1d54c",
+    "url": "/ZITOUNIMED/monsters-roloxed.git/static/css/main.d84d236b.chunk.css"
+  },
+  {
+    "revision": "65f1979df10a79bff5fc",
+    "url": "/ZITOUNIMED/monsters-roloxed.git/static/js/2.66a46975.chunk.js"
+  },
+  {
+    "revision": "bd6c746c027d38b1d54c",
+    "url": "/ZITOUNIMED/monsters-roloxed.git/static/js/main.89b394e6.chunk.js"
+  },
+  {
+    "revision": "7fe6cf1a311106eb7c6d",
+    "url": "/ZITOUNIMED/monsters-roloxed.git/static/js/runtime~main.d77181ff.js"
+  }
+]);
